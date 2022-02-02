2e110fb7f44bc75f3e877c197db2df8b8a1c1947
James Metz <jamesmetz1231@gmail.com>
2022-02-02 15:40:55 -0500

    Create wxw_dino_run.cpp

---
+++ wxw_dino_run.cpp
@@ -0,0 +1,2 @@
+# this is intended to be a cpp program based on the wxWidgets library 
+# the end goal is to deploy a simple game across all my computers and packaged in a way that can be easily transfered. 
