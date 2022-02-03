fa2575457fa46654fd8cf791b0e4510c4bacef39
James Metz <jamesmetz1231@gmail.com>
2022-02-02 20:25:17 -0500

    Reworking the directory to make space for the next two projects.
    I am making a cpp file for a terminal based running game and another file
    for the gui version of the same concept.
    
    The first will be using strings as the primary data type, the second will use the wxWidget library.

--- wxw_dino_run.cpp
+++ wxw_dino_run.cpp
@@ -1,2 +1,8 @@
-# this is intended to be a cpp program based on the wxWidgets library 
-# the end goal is to deploy a simple game across all my computers and packaged in a way that can be easily transfered. 
+// this is intended to be a cpp program based on the wxWidgets library 
+// the end goal is to create a functional game that can be ran from the 
+// primary app (terminal_main.cpp) 
+// this whole program is essentially a test, only if this works will i worry about implementation. 
+
+
+
+
