4b1217ee5ab54f33da24ddfe144fe50c4bcd26be
James Metz <jamesmetz1231@gmail.com>
2022-02-02 23:29:18 -0500

    Adding template and test .txt file.

---
+++ terminal_runner.cpp
@@ -0,0 +1,53 @@
+// this is a terminal based precursor to the gui runner game I intend to make.
+// its also a good opportunity to get used to working with strings. I could use
+// other data types but this should make things quite easy to print.
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+  // primary display strings
+  string d_str_1;
+  string d_str_2;
+  string d_str_3;
+  string d_str_4;
+  string d_str_5;
+  string d_str_6;
+  string d_str_7;
+  string d_str_8;
+  string d_str_9;
+  string d_str_10;
+
+// starting image and test template
+d_str_1 = "____________________________________________________________________________________________________";
+d_str_2 = "     @@@       .                                .                 .                  @@@            ";
+d_str_3 = "    @@@@@                           @@@                                             @@@@@           ";
+d_str_4 = "                                   @@@@@              .                              @@@            ";
+d_str_5 = "                                                                                           .        ";
+d_str_6 = ".                                                                             .                     ";
+d_str_7 = "       0                     .                                  *$*                                 ";
+d_str_8 = "      /|\                              *$*                      *$*                   *$*    .      ";
+d_str_9 = "      / \                              *$*      .               *$*              .    *$*           ";
+d_str_10= "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+
+
+
+
+
+
+
+  // test
+  cout << d_str_1 << endl;
+  cout << d_str_2 << endl;
+  cout << d_str_3 << endl;
+  cout << d_str_4 << endl;
+  cout << d_str_5 << endl;
+  cout << d_str_6 << endl;
+  cout << d_str_7 << endl;
+  cout << d_str_8 << endl;
+  cout << d_str_9 << endl;
+  cout << d_str_10 << endl;
+
+}
